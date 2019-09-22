a5aa3cbf4b1879cd35df3341f6ac53b0f5283231
Projjol Banerji <probaner23@gmail.com>
2019-09-22 06:04:33 -0400

    expanded list view

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,6 @@ class _MyApp extends State<MyApp> {
   final FirebaseMessaging firebaseMessaging = FirebaseMessaging();
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     firebaseMessaging.getToken().then((token){
       print(token);
@@ -35,13 +34,6 @@ class _MyApp extends State<MyApp> {
       },
       onResume: (Map<String, dynamic> message) async {
         print("entered onResume");
-        print("literally anything");
-        // print(context);
-        // Navigator.push(
-        //   context, 
-        //   MaterialPageRoute(builder: (context) => SecondRoute()),
-        // );
-        // print("lol wtf");
       }
     );
   }
@@ -52,38 +44,52 @@ class _MyApp extends State<MyApp> {
       theme: ThemeData(
         primaryColor: Colors.orange[700],
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('🔥 Alert'),
-        ),
-        body: Center(
-          child: Card(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                const ListTile(
-                  leading: Icon(Icons.priority_high),
-                  title: Text('WARNING: You are in a high risk area for a forest fire!', style: TextStyle(color: Colors.red)),
-                  subtitle: Text('Please consider evacuating immediately!')
+      home: MyHome()
+    );
+  }
+}
+
+class MyHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.orange[100],
+      appBar: AppBar(
+        title: Text('🔥 Alert'),
+      ),
+      body: Center(
+        child: Card(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              const ListTile(
+                leading: Icon(Icons.priority_high),
+                title: Text('WARNING: You are in a high risk area for a forest fire!', style: TextStyle(color: Colors.red)),
+                subtitle: Text('Please read our community driven guidelines for how to best protect yourself')
+              ),
+              ButtonTheme.bar(
+                child: ButtonBar(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    FlatButton(
+                      child: const Text('IMMEDIATE ACTIONS'),
+                      onPressed: () {
+                        navigateToSubPage(context);
+                      },
+                    ),
+                    // FlatButton(
+                    //   child: const Text('ONLINE HELP'),
+                    //   onPressed: _launchURL,
+                    // ),
+                    FlatButton(
+                      child: const Text('REPORT ACTIVITY'),
+                      onPressed: () { /* ... */ },
+                    )
+                  ]
                 ),
-                ButtonTheme.bar(
-                  child: ButtonBar(
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      FlatButton(
-                        child: const Text('ONLINE HELP'),
-                        onPressed: _launchURL,
-                      ),
-                      FlatButton(
-                        child: const Text('REPORT ACTIVITY'),
-                        onPressed: () { /* ... */ },
-                      )
-                    ]
-                  ),
-                )
-              ]
-            ),
-          )
+              )
+            ]
+          ),
         )
       )
     );
@@ -99,37 +105,92 @@ _launchURL() async {
   }
 }
 
-class SecondRoute extends StatelessWidget {
+// stores ExpansionPanel state information
+class Item {
+  Item({
+    this.expandedValue,
+    this.headerValue,
+    this.isExpanded = false,
+  });
+
+  String expandedValue;
+  String headerValue;
+  bool isExpanded;
+}
+
+// item generator
+List<Item> generateItems(int numberOfItems) {
+  return List.generate(numberOfItems, (int index) {
+    return Item(
+      headerValue: 'Panel $index',
+      expandedValue: 'This is item number $index',
+    );
+  });
+}
+
+List<Item> instructionSet = [
+  Item(headerValue: 'What does high risk mean?', expandedValue: 'The weather conditions are optimal for a fast-growing wildfire to appear; burning activities should be restricted.'),
+  Item(headerValue: 'What do I need to do?', expandedValue: 'Increase your alertness, look out for wildfire signals; Be ready to REPORT through the App or call 911 if you notice unattended or out-of-control fire.'),
+  Item(headerValue: 'Should I evacuate my house?', expandedValue: 'Please wait for further instructions from the police')
+];
+
+// widget that will hold list items
+class MyStatefulWidget extends StatefulWidget {
+  MyStatefulWidget({Key key}) : super(key: key);
+
+  @override
+  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
+}
+
+// state for list item holder
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  List<Item> _data = instructionSet;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.orange[100],
       appBar: AppBar(
-        title: Text("Second Route"),
+        title: Text('🔥 Alert'),
       ),
       body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            const SizedBox(height: 30),
-            RaisedButton(
-              onPressed: () {},
-              child: const Text(
-                'wassup danger',
-                style: TextStyle(fontSize: 20)
-              ),
-              color: Colors.orange[400],
-              highlightColor: Colors.orange[100],
-              padding: EdgeInsets.symmetric(
-                horizontal: 18.0, 
-                vertical: 10.0
-              ),
-              shape: new RoundedRectangleBorder(
-                borderRadius: new BorderRadius.circular(20.0),
-                side: BorderSide(color: Colors.orange[100]))
-            )
-          ],
-        ),
-      ),
+        child: SingleChildScrollView(
+          child: Container(
+            child: _buildPanel(),
+          ),
+        )
+      )
     );
   }
+
+  Widget _buildPanel() {
+    return ExpansionPanelList(
+      expansionCallback: (int index, bool isExpanded) {
+        setState(() {
+          _data[index].isExpanded = !isExpanded;
+        });
+      },
+      children: _data.map<ExpansionPanel>((Item item) {
+        return ExpansionPanel(
+          headerBuilder: (BuildContext context, bool isExpanded) {
+            return ListTile(
+              title: Text(item.headerValue),
+            );
+          },
+          body: ListTile(
+              title: Text(item.expandedValue),
+              onTap: () {
+                setState(() {
+                  _data.removeWhere((currentItem) => item == currentItem);
+                });
+              }),
+          isExpanded: item.isExpanded,
+        );
+      }).toList(),
+    );
+  }
+}
+
+Future navigateToSubPage(context) async {
+  Navigator.push(context, MaterialPageRoute(builder: (context) => MyStatefulWidget()));
 }
