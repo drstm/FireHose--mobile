53e49f121fa5e131f9e1bbe4325ca9206e662896
Projjol Banerji <probaner23@gmail.com>
2019-09-22 04:31:39 -0400

    opens browser

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:url_launcher/url_launcher.dart';
+
 // import 'package:http/http.dart' as http;
 
 void main() => runApp(MyApp());
@@ -24,6 +26,24 @@ class _MyApp extends State<MyApp> {
     firebaseMessaging.getToken().then((token){
       print(token);
     });
+    firebaseMessaging.configure(
+      onLaunch: (Map<String, dynamic> message) async {
+        print("entered onLaunch");
+      },
+      onMessage: (Map<String, dynamic> message) async {
+        print("entered onMessage");
+      },
+      onResume: (Map<String, dynamic> message) async {
+        print("entered onResume");
+        print("literally anything");
+        // print(context);
+        // Navigator.push(
+        //   context, 
+        //   MaterialPageRoute(builder: (context) => SecondRoute()),
+        // );
+        // print("lol wtf");
+      }
+    );
   }
   @override
   Widget build(BuildContext context) {
@@ -37,30 +57,79 @@ class _MyApp extends State<MyApp> {
           title: Text('🔥 Alert'),
         ),
         body: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              const SizedBox(height: 30),
-              RaisedButton(
-                onPressed: () {},
-                child: const Text(
-                  'Fetch Weather Conditions',
-                  style: TextStyle(fontSize: 20)
+          child: Card(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                const ListTile(
+                  leading: Icon(Icons.priority_high),
+                  title: Text('WARNING: You are in a high risk area for a forest fire!', style: TextStyle(color: Colors.red)),
+                  subtitle: Text('Please consider evacuating immediately!')
                 ),
-                color: Colors.orange[400],
-                highlightColor: Colors.orange[100],
-                padding: EdgeInsets.symmetric(
-                  horizontal: 18.0, 
-                  vertical: 10.0
-                ),
-                shape: new RoundedRectangleBorder(
-                  borderRadius: new BorderRadius.circular(20.0),
-                  side: BorderSide(color: Colors.orange[100]))
-              )
-            ],
-          ),
+                ButtonTheme.bar(
+                  child: ButtonBar(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      FlatButton(
+                        child: const Text('ONLINE HELP'),
+                        onPressed: _launchURL,
+                      ),
+                      FlatButton(
+                        child: const Text('REPORT ACTIVITY'),
+                        onPressed: () { /* ... */ },
+                      )
+                    ]
+                  ),
+                )
+              ]
+            ),
+          )
         )
       )
+    );
+  }
+}
+
+_launchURL() async {
+  const url = 'https://flutter.io';
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
+  }
+}
+
+class SecondRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Second Route"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            const SizedBox(height: 30),
+            RaisedButton(
+              onPressed: () {},
+              child: const Text(
+                'wassup danger',
+                style: TextStyle(fontSize: 20)
+              ),
+              color: Colors.orange[400],
+              highlightColor: Colors.orange[100],
+              padding: EdgeInsets.symmetric(
+                horizontal: 18.0, 
+                vertical: 10.0
+              ),
+              shape: new RoundedRectangleBorder(
+                borderRadius: new BorderRadius.circular(20.0),
+                side: BorderSide(color: Colors.orange[100]))
+            )
+          ],
+        ),
+      ),
     );
   }
 }
